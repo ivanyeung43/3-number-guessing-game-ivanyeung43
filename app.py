55d8515cda5aa685b2dc9117a78b0718dc7a3e4f
ivanyeung43 <ivany43@nycstudents.net>
2026-02-10 08:54:24 -0500

    Update app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+# Number Guessing Game
+
+
+""" count = 1
+
+while count <= 5:
+    print("This is loop number", count)
+    count = count + 1
+ """
+""" order = ""
+
+while order != "done":
+    order = input("What would you like to order? (type 'done' to finish): ")
+
+print("Thanks for your order!") """
+
+""" count = 10
+while count >=1:
+    print("This is loop number", count)
+    count = count-1 """
+
+favorite_color = ""
+
+while favorite_color != "stop":
+    favorite_color = input("What is your favorite color? (Type Stop to finish):")
+print("Okay")
